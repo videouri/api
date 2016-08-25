c9201bb0d4d0e00710912006e0354af102d5bee8
Alex Budurovici <w0rldart@w0rldart.com>
2016-08-25 04:07:02 +0000

    Quick improvement to show error message

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,17 +2,18 @@
 @extends('app')
 
 @section('content')
-<br/>
-<div class="container">
-    <div class="row">
-        <div class="col s12">
-            <h1>Sorry, but there was an internal error.</h1>
-            <p>
-                For some reason, the request could not be completed,
-                due to an internal malfunction. Please,
-                do let us know if this occurs to you again, and we'll try to fix it.
-            </p>
+    <br/>
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h2>Sorry, but there was an internal error.</h2>
+                {{ $exception->getMessage() }}
+                <p>
+                    For some reason, the request could not be completed,
+                    due to an internal malfunction. Please,
+                    do let us know if this occurs to you again, and we'll try to fix it.
+                </p>
+            </div>
         </div>
     </div>
-</div>
 @endsection
