21e227a9ae45f25c94f544778399d41d2b927dd5
Alex Budurovici <w0rldart@w0rldart.com>
2016-09-26 08:31:20 +0000

    Adapt VideoController for the new Scout service and other improvements

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,11 +2,13 @@
 
 namespace Videouri\Http\Controllers;
 
-use Videouri\Http\Requests;
-use Videouri\Services\ApiFetcher;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Redirector;
 use Illuminate\View\View;
+use Videouri\Entities\Source;
+use Videouri\Services\Scout\Scout;
+use Videouri\Services\Transformer\Transform;
+use Videouri\Services\Transformer\VideoTransformer;
 
 /**
  * @package Videouri\Http\Controllers
@@ -14,16 +16,13 @@ use Illuminate\View\View;
 class VideoController extends Controller
 {
     /**
-     * @var ApiFetcher
+     * @var Scout
      */
-    protected $apiFetcher;
+    private $scout;
 
-    /**
-     * @param ApiFetcher $apiFetcher
-     */
-    public function __construct(ApiFetcher $apiFetcher)
+    public function __construct()
     {
-        $this->apiFetcher = $apiFetcher;
+        $this->scout = app('videouri.scout');
     }
 
     /**
@@ -37,27 +36,27 @@ class VideoController extends Controller
     /**
      * Display the specified resource.
      *
-     * @var int $id
-     * @var string $slug
+     * @param string $slug
+     * @param string $customId
      *
      * @return View
      */
-    public function show($customId, $slug = null)
+    public function show($slug, $customId)
     {
         $api = substr($customId, 1, 1);
         $originalId = substr_replace($customId, '', 1, 1);
 
         switch ($api) {
             case 'd':
-                $api = 'Dailymotion';
+                $api = Source::DAILYMOTION;
                 break;
 
             case 'v':
-                $api = 'Vimeo';
+                $api = Source::VIMEO;
                 break;
 
             case 'y':
-                $api = 'Youtube';
+                $api = Source::YOUTUBE;
                 break;
 
             default:
@@ -65,27 +64,19 @@ class VideoController extends Controller
                 break;
         }
 
-        # Return cached video or fetch it new
-        if (!$video = $this->fetcher->getVideoInfo($api, $originalId)) {
-            return abort(404);
-        }
+        $video = $this->scout->getVideo($api, $originalId);
 
-        $data['thumbnail'] = $video['thumbnail'];
+        $video = Transform::item($video, new VideoTransformer());
+
         $data['video'] = json_encode($video);
-
-        $relatedVideos = $this->fetcher->getRelatedVideos($api, $video['original_id']);
-        if (!empty($relatedVideos)) {
-            $relatedVideos = json_encode($relatedVideos);
-        }
-
-        $data['recommended'] = $relatedVideos;
+        $data['thumbnail'] = $video['data']['thumbnail'];
 
         // Metadata
-        $data['title'] = $video['title'] . ' - Videouri';
-        $data['description'] = str_limit($video['description'], 100);
+        $data['title'] = $video['data']['title'] . ' - Videouri';
+        $data['description'] = str_limit($video['data']['description'], 100);
         $data['canonical'] = 'video/' . $video['custom_id'];
         $data['bodyId'] = 'videoPage';
 
-        return view('videouri.public.video', $data);
+        return view('public.video', $data);
     }
 }
