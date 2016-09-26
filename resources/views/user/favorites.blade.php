c0ad174c985a67b7c7f4141ef9cb1c7cec556dd9
Alex Budurovici <w0rldart@w0rldart.com>
2016-09-26 09:07:20 +0100

    Change favourites to favorites

--- resources/views/user/favorites.blade.php
+++ resources/views/user/favorites.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h1 class="flow-text">Favorited videos</h1>
+        <h1 class="flow-text">Favorite videos</h1>
         <videos-list content="favorites"></videos-list>
     </div>
 @endsection
