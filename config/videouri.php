7afc51cfffe2acb3df769b5388d631da097eda6e
Alex Budurovici <w0rldart@w0rldart.com>
2016-09-26 23:34:50 +0100

    Fix typos in meta tags

--- config/videouri.php
+++ config/videouri.php
@@ -2,10 +2,10 @@
 
 return [
     'default' => [
-        'title'       => 'Videouri.com, your friendly neighborhood video search engine',
-        
-        'description' => 'Search and watch funny videos, top rated videos, most viewed videos, scarry videos... all sorts of videos',
+        'title' => 'Videouri.com, your friendly neighborhood video search engine',
 
-        'keywords'    => 'videoclips,funny videos,scarry videos,top rated videos,most viewed videos,jokes,pranks,videouri',
+        'description' => 'Search and watch funny videos, top rated videos, most viewed videos, scary videos... all sorts of videos',
+
+        'keywords' => 'video clips,funny videos, scary videos,top rated videos,most viewed videos,jokes,pranks,videouri',
     ]
 ];
